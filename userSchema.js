0a681ea588751e85573dd5d5d576bc4cb3980021
kshitijk83 <kshitijk83@gmail.com>
2019-09-24 23:46:37 +0530

    merge

--- userSchema.js
+++ userSchema.js
@@ -3,7 +3,7 @@ var Schema=mongoose.Schema;
 var userSchema=new Schema({
 	  name:{type:String,required:true},
 	  team:{type:String,required:true},
-	  email:{type:String,required:true,unique:true},
+	  email:{type:String,required:true},
       phone:{type:Number,required:true},
       college:{type:String,required:true},
       city:{type:String,required:true},
