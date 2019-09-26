b4a3050e3ede2e16a7d152455d18e8b7f9489b7d
kshitijk83 <kshitijk83@gmail.com>
2019-09-26 16:42:57 +0530

    test

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,25 @@
-window.addEventListener('DOMContentLoaded', e=>{
-  var toggleBtn = document.querySelector('#nav-icon2');
-  var menu = document.querySelectorAll('.menu');
-  var navbarActive = false;
-  toggleBtn.addEventListener('click', ()=>{
-    if(!navbarActive){
-      menu[0].style.left = '0';
-      toggleBtn.classList.add('open');
-      navbarActive =true;
-      // window.addEventListener('scroll', noScroll);
-    }  else{
-      navbarActive=false;
-      menu[0].style.left = '-100%';
-      toggleBtn.classList.remove('open');
-      // window.removeEventListener('scroll', noScroll);
-    }
-  })
-})
+// window.addEventListener('DOMContentLoaded', e=>{
+//   var toggleBtn = document.querySelector('#nav-icon2');
+//   var menu = document.querySelectorAll('.menu');
+//   var navbarActive = false;
+//   toggleBtn.addEventListener('click', ()=>{
+//     if(!navbarActive){
+//       console.log('clicked open');
+//       menu[0].style.left = '0';
+//       toggleBtn.classList.add('open');
+//       navbarActive =true;
+//       // window.addEventListener('scroll', noScroll);
+//     }  else{
+//       console.log('clicked close');
 
-function noScroll() {
-  window.scrollTo(0, 0);
-}
+//       navbarActive=false;
+//       menu[0].style.left = '-100%';
+//       toggleBtn.classList.remove('open');
+//       // window.removeEventListener('scroll', noScroll);
+//     }
+//   })
+// })
+
+// function noScroll() {
+//   window.scrollTo(0, 0);
+// }
