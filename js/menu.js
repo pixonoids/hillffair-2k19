165840a51d84ab5b9eacb796260bea9ebe2f74a3
kshitijk83 <kshitijk83@gmail.com>
2019-09-26 17:12:04 +0530

    minor 2

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,42 @@
-window.addEventListener('DOMContentLoaded', e=>{
-  var toggleBtn = document.querySelector('#nav-icon2');
-  var menu = document.querySelectorAll('.menu');
-  var navbarActive = false;
-  toggleBtn.addEventListener('click', ()=>{
-    if(!navbarActive){
-      console.log('clicked open');
-      menu[0].style.left = '0';
-      toggleBtn.classList.add('open');
-      navbarActive =true;
-      // window.addEventListener('scroll', noScroll);
-    }  else{
-      console.log('clicked close');
+// window.addEventListener('DOMContentLoaded', e=>{
+//   var toggleBtn = document.querySelector('#nav-icon2');
+//   var menu = document.querySelectorAll('.menu');
+//   var navbarActive = false;
+//   toggleBtn.addEventListener('click', ()=>{
+//     if(!navbarActive){
+//       console.log('clicked open');
+//       menu[0].style.left = '0';
+//       toggleBtn.classList.add('open');
+//       navbarActive =true;
+//       // window.addEventListener('scroll', noScroll);
+//     }  else{
+//       console.log('clicked close');
 
-      navbarActive=false;
-      menu[0].style.left = '-100%';
-      toggleBtn.classList.remove('open');
-      // window.removeEventListener('scroll', noScroll);
-    }
-  })
-})
+//       navbarActive=false;
+//       menu[0].style.left = '-100%';
+//       toggleBtn.classList.remove('open');
+//       // window.removeEventListener('scroll', noScroll);
+//     }
+//   })
+// })
 
-// function noScroll() {
-//   window.scrollTo(0, 0);
+// // function noScroll() {
+// //   window.scrollTo(0, 0);
+// // }
+
+// function navbar(){
+//   if(!navbarActive){
+//     console.log('clicked open');
+//     menu[0].style.left = '0';
+//     toggleBtn.classList.add('open');
+//     navbarActive =true;
+//     // window.addEventListener('scroll', noScroll);
+//   }  else{
+//     console.log('clicked close');
+
+//     navbarActive=false;
+//     menu[0].style.left = '-100%';
+//     toggleBtn.classList.remove('open');
+//     // window.removeEventListener('scroll', noScroll);
+//   }
 // }
